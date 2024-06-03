e55ba26e3f53eed0496ea9b6483c5371135d80b6
Sebastian Fabian <sesseupires10@gmail.com>
2024-06-03 08:25:48 -0300

    projeto integrador

---
+++ controllers/usercontroller.js
@@ -0,0 +1,40 @@
+// userController.js
+const User = require('../models/user');
+
+exports.createUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = await User.create({ username, password });
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Outros controladores para autenticação
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+exports.authenticateUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Procura o usuário no banco de dados
+    const user = await User.findOne({ username });
+    // Se o usuário não existe, retorna erro
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    // Verifica se a senha está correta
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+    // Gera um token JWT
+    const token = jwt.sign({ userId: user._id }, 'secret_key');
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
